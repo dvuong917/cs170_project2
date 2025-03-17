8f3ac64d9754058e284e85439ed4ab30e14aa1a0
dvuong917 <dvuongwm@gmail.com>
2025-03-17 00:38:19 -0700

    forward search done

--- main.py
+++ main.py
@@ -1,84 +1,153 @@
 import numpy as np
-import random
-
-def nearest_neighbor(test_point, classes, features):
-    min_dist = np.inf
-    predicted_class = None
-    for row, feature in enumerate(features): # compare test point features with dataSet features
-        dist = distance(test_point, feature)
-        if (dist < min_dist):
-            min_dist = dist
-            predicted_class = classes[row] # copy class of nearest neighbor
-            print("New neighbor closest from row: ", row+1)
-    return predicted_class
+import time
+# import random
 
 def distance(point1, point2):
     return np.sqrt(np.sum((np.array(point1) - np.array(point2))**2))
 
-print("Welcome to Dylan Vuong's Feature Selection Algorithm.")
-# inputFile = input("Type in the name of the file to test: ")
-inputFile = "CS170_Small_Data__20.txt"
-array = np.loadtxt(inputFile)
-# classes = array[:, 0].astype(int) # first column
-# features = array[:, 1:] # the rest of the columns
-# numRows = array.shape[0]
-# numColumns = array.shape[1]
-# print("numRows = ", numRows)
-# print("numColumns = ", numColumns)
+# def leave_one_out_rand_stub(data, current_set, feature_to_add): # testing stub
+#     return random.randint(0,100)
 
-def leave_one_out_rand_stub(data, current_set, feature_to_add): # testing stub
-    return random.randint(0,100)
+def leave_one_out_cross_validation(data, current_set, feature_to_add):
+    numRows = data.shape[0]
+    numColumns = data.shape[1]
+    number_correctly_classified = 0
 
-def leave_one_out_cross_validation(array):
-    numRows = array.shape[0]
-    numColumns = array.shape[1]
-    for i in range(1, 10): # i = row
+    selected_features = current_set + [feature_to_add] # use brackets since current_set is list and feature_to_add is int
+
+    # make local copy of dataset with ignored features
+    data_copy = data[:, [0] + [x+1 for x in selected_features]] # +1 to avoid class label column
+
+    for i in range(numRows): # change to numRows
+        object_to_classify = data_copy[i, 1:]
+        label_object_to_classify = data_copy[i, 0]
+
         nearest_neighbor_dist = np.inf
-        nearest_neighbor_loc = np.inf
-        object_to_classify = array[i-1, i]
-        classes = array[i-1, 0]
-        for k in range(1, numColumns):
+
+        for k in range(numRows): # change to numRows
             if k != i: # don't compare to yourself
-                other_objects = array[i-1, k]
-                print("Ask if", i, "is nearest neighbor with", k)
-                print("object to classify = ", object_to_classify)
-                print("other objects = ", other_objects)
-                dist = distance(object_to_classify, other_objects)
+                # print("Ask if", i, "is nearest neighbor with", k)
+                # print("object to classify = ", object_to_classify)
+                # print("other objects = ", other_objects)
+                dist = distance(object_to_classify, data_copy[k, 1:])
                 if (dist < nearest_neighbor_dist):
                     nearest_neighbor_dist = dist
-                    predicted_class = classes
-
+                    nearest_neighbor_label = data_copy[k, 0]
         # print("Looping over i, at the", i, "location")
         # print("The", i, "th object is in class", classes)
 
-def feature_search_demo(data, numColumns):
+        # print("Object", i+1, "is class", label_object_to_classify)
+        # print("Its nearest_neighbor is", nearest_neighbor_loc+1, "which is in class", nearest_neighbor_label)
+
+        if label_object_to_classify == nearest_neighbor_label:
+            number_correctly_classified += 1
+    
+    accuracy = number_correctly_classified / numRows
+    # print(f"Selected features: {selected_features}")
+    # print("Accuracy = ", accuracy*100, "%")
+    return accuracy
+
+
+def forward_feature_search_demo(data, numColumns):
     current_set_of_features = [] # initialize empty set
-    for i in range(1, numColumns):
-        print("On the", i, "th level of the search tree")
-        feature_to_add_at_this_level = []
+    overall_best_accuracy = 0
+    overall_best_set = []
+    for i in range(numColumns):
+        # print("On the", i+1, "th level of the search tree")
+        feature_to_add_at_this_level = None
         best_so_far_accuracy = 0
-        for k in range(1, numColumns):
+        for k in range(numColumns):
             if (k not in current_set_of_features):
-                print("--Considering adding the", k, "feature")
-                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k+1)
+                # print("--Considering adding the", k+1, "feature")
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
+                rounded_accuracy = round(accuracy * 100, 5)
+                print(f"    Using feature(s) {[x+1 for x in current_set_of_features] + [k+1]}, accuracy is {rounded_accuracy}%")
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
                     feature_to_add_at_this_level = k
+        print()
+
         current_set_of_features.append(feature_to_add_at_this_level)
-        print("On level", i, ", added feature", feature_to_add_at_this_level, "to current set")
+        if best_so_far_accuracy > overall_best_accuracy:
+            overall_best_accuracy = best_so_far_accuracy
+            overall_best_set = current_set_of_features.copy()
+        else:
+            print("(Warning, accuracy has decreased! Continuing search in case of local maxima.)")
 
-#feature_search_demo(array, numColumns)
-leave_one_out_cross_validation(array)
+        # print(f"OVERALL BEST SET: {[x+1 for x in overall_best_set]}")
+        # print("On level", i+1, ", added feature", feature_to_add_at_this_level+1, "to current set")
+        rounded_best_so_far = round(best_so_far_accuracy * 100, 5)
+        print(f"Feature set {[x+1 for x in current_set_of_features]} was best, accuracy is {rounded_best_so_far}%\n")
 
-# # Test distance 
-# testset = np.array([[0,2,2]])
-# testfeature = testset[:, 1:]
-# testpoint = np.array([1, 4])
-# print("Distance: ", distance(testpoint, testfeature))
+    rounded_overall = round(overall_best_accuracy * 100, 5)
+    print(f"Finished search! The best feature subset is {[x+1 for x in overall_best_set]}, which has an accuracy of {rounded_overall}%\n")
 
-# # Test nearest neighbor
-# testpoint1 = np.array([2.3515754e+00,   1.0158324e+00,  -2.1784832e+00,   1.5897041e-01,  -6.6376666e-01,  -2.8055787e-02]) # from row 18
-# testpoint2 = np.array([-1.1075231e+00,   1.2113223e+00,  -8.6869643e-01,  -1.1616665e+00,  -1.4904901e+00,  -1.1996686e+00]) # from row 1
-# print("(SHOULD RETURN 2) Predicted class: ", nearest_neighbor(testpoint1, classes, features))
-# print("(SHOULD RETURN 2) Predicted class: ", nearest_neighbor(testpoint2, classes, features))
+def backward_feature_search_demo(data, numColumns): # TODO 
+    current_set_of_features = [] # initialize empty set
+    overall_best_accuracy = 0
+    overall_best_set = []
+    for i in range(numColumns):
+        # print("On the", i+1, "th level of the search tree")
+        feature_to_add_at_this_level = None
+        best_so_far_accuracy = 0
+        for k in range(numColumns):
+            if (k not in current_set_of_features):
+                # print("--Considering adding the", k+1, "feature")
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
+                rounded_accuracy = round(accuracy * 100, 5)
+                print(f"    Using feature(s) {[x+1 for x in current_set_of_features] + [k+1]}, accuracy is {rounded_accuracy}%")
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_add_at_this_level = k
+        print()
 
+        current_set_of_features.append(feature_to_add_at_this_level)
+        if best_so_far_accuracy > overall_best_accuracy:
+            overall_best_accuracy = best_so_far_accuracy
+            overall_best_set = current_set_of_features.copy()
+        else:
+            print("(Warning, accuracy has decreased! Continuing search in case of local maxima.)")
+
+        # print(f"OVERALL BEST SET: {[x+1 for x in overall_best_set]}")
+        # print("On level", i+1, ", added feature", feature_to_add_at_this_level+1, "to current set")
+        rounded_best_so_far = round(best_so_far_accuracy * 100, 5)
+        print(f"Feature set {[x+1 for x in current_set_of_features]} was best, accuracy is {rounded_best_so_far}%\n")
+
+    rounded_overall = round(overall_best_accuracy * 100, 5)
+    print(f"Finished search! The best feature subset is {[x+1 for x in overall_best_set]}, which has an accuracy of {rounded_overall}%\n")
+
+
+def main():
+    print("Welcome to Dylan Vuong's Feature Selection Algorithm.")
+    # inputFile = input("Type in the name of the file to test: ")
+    inputFile = "CS170_Small_Data__11.txt"
+    # inputFile = "CS170_Large_Data__12.txt"
+    data = np.loadtxt(inputFile)
+    numRows = data.shape[0]
+    numColumns = data.shape[1]
+    
+    print("\nType the number of the algorithm you want to run.")
+
+    menuOn = True
+    while (menuOn):
+        start_time = time.time()
+        choice = int(input("\n1) Forward Selection\n2) Backward Elimination\n"))
+        if choice == 1:
+            print(f"\nThis dataset has {numColumns-1} features (not including the class), with {numRows} instances.\n")
+            start_time = time.time()
+            forward_feature_search_demo(data, numColumns-1)
+            end_time = time.time()
+            print(f"Running time: {(end_time - start_time):.1f} seconds")
+            break
+        elif choice == 2:
+            print(f"\nThis dataset has {numColumns-1} features (not including the class), with {numRows} instances.\n")
+            start_time = time.time()
+            backward_feature_search_demo(data, numColumns-1)
+            end_time = time.time()
+            print(f"Running time: {(end_time - start_time):.1f} seconds")
+            break
+        else:
+            print("Invalid choice.")
+            continue
+        
+main()
